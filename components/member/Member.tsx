6e34428aae019539fa3a328e8457b8e8f0b82f46
HanKyeon <hgh21233@gmail.com>
2024-11-30 23:08:55 +0900

    feat: header 모달 추가

--- components/member/Member.tsx
+++ components/member/Member.tsx
@@ -1,17 +1,30 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useOverlay } from '@/hooks/useOverlay';
 import MemberListHeader from './header/MemberListHeader';
-import { MemberRecord, type DefaultMemberRecord } from '@/types/type';
 import MemberTable from './table/MemberTable';
-import { setRecordsInLocalStorage } from '../utils/storage-utils';
 import { MemberProvider } from '@/stores/member-store';
 
+import MemberFormModal from './add-member-form/MemberFormModal';
+
 const Member = function () {
+  const { open, openHandler, closeHandler } = useOverlay();
   return (
     <MemberProvider>
+      <MemberFormModal
+        open={open}
+        closeHandler={closeHandler}
+        onSubmit={data => {
+          console.log(data);
+          // closeHandler();
+        }}
+      />
       <section className="w-full h-full flex flex-col flex-shrink-0">
-        <MemberListHeader title="회원 목록" buttonText="추가" />
+        <MemberListHeader
+          title="회원 목록"
+          buttonText="추가"
+          buttonClick={openHandler}
+        />
         <main className="w-full flex-1">
           <MemberTable />
         </main>
