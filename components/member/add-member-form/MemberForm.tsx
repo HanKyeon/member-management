64bce4d5c790ba99a81d98f340bad4caa82e6c82
HanKyeon <hgh21233@gmail.com>
2024-11-30 23:20:31 +0900

    feat: add form modal main
    - 재사용성 향상, 확장 용이에 집중
    - name만 있으면 formContext를 관리할 수 있는 react-hook-form의 특징을 살림
    - DEFAULT_COLS에 필요한 Field를 추가하고 ElementMap에 해당 Filed의 이름에 맞게 추가하면 확장이 쉬움.

---
+++ components/member/add-member-form/MemberForm.tsx
@@ -0,0 +1,66 @@
+import { DEFAULT_COLS, DEFAULT_JOBS } from '@/components/constant/value';
+import { MemberRecord } from '@/types/type';
+import { createElement, PropsWithChildren } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import FormInput from '../../ui/hook-form-inputs/FormInput';
+import FormTextArea from '@/components/ui/hook-form-inputs/FormTextArea';
+import FormDate from '@/components/ui/hook-form-inputs/FormDatePicker';
+import FormSelect from '@/components/ui/hook-form-inputs/FormSelect';
+import { formatDate } from '@/utils/date-utils';
+import FormCheckBox from '@/components/ui/hook-form-inputs/FormCheck';
+
+interface Props {
+  onSubmit: (member: MemberRecord) => void;
+  member?: MemberRecord;
+}
+
+const ElementMap = {
+  name: FormInput,
+  address: FormInput,
+  memo: FormTextArea,
+  joinDate: FormDate,
+  job: FormSelect,
+  emailAgreement: FormCheckBox,
+};
+
+/**
+ * @param {React.ReactNode} children footer로 사용됩니다.
+ */
+const MemberForm = function ({
+  children,
+  onSubmit,
+  member,
+}: PropsWithChildren<Props>) {
+  const methods = useForm<MemberRecord>({
+    defaultValues: {
+      checked: member?.checked ?? false,
+      name: member?.name ?? '',
+      address: member?.address ?? '',
+      memo: member?.memo ?? '',
+      joinDate: member?.joinDate ?? formatDate(new Date()),
+      job: member?.job ?? DEFAULT_JOBS[0].value,
+      emailAgreement: member?.emailAgreement ?? false,
+    },
+  });
+  const { handleSubmit } = methods;
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <main className="w-full px-[24px] pt-[10px] pb-[20px] gap-[20px] flex flex-col">
+          {DEFAULT_COLS.map(col => {
+            return createElement(ElementMap[col.filterKey], {
+              key: `form-block-${col.filterKey}`,
+              name: col.filterKey,
+              label: col.name,
+              required: col.required,
+              className: col.className,
+            });
+          })}
+        </main>
+        {children}
+      </form>
+    </FormProvider>
+  );
+};
+
+export default MemberForm;
