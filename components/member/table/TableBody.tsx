939ce119c775e406efff632953f521af1db8ea06
HanKyeon <hgh21233@gmail.com>
2024-11-30 19:33:16 +0900

    feat: 테이블 몸체 부분 데이터 연결
    - selected 등 데이터와 연결 적용
    - DEFAULT_COLS를 통해 코드 간소화

--- components/member/table/TableBody.tsx
+++ components/member/table/TableBody.tsx
@@ -1,11 +1,12 @@
 import { textBaseNormal } from '@/components/constant/style';
 import { tableWidth } from '@/components/constant/table';
+import { DEFAULT_COLS } from '@/components/constant/value';
 import Button from '@/components/ui/Button';
 import CheckBox from '@/components/ui/ChechBox';
 import { useMember } from '@/stores/member-store';
 
 const TableBody = function () {
-  const { members } = useMember();
+  const { members, checkMember, checkEmailMember } = useMember();
   return (
     <tbody>
       {members.map((member, idx) => (
@@ -16,37 +17,29 @@ const TableBody = function () {
           <th
             className={`${tableWidth[0]} px-[8px] border-r-[1px] border-recatch-split py-[13px]`}
           >
-            <CheckBox />
+            <CheckBox
+              checked={member.checked}
+              onChange={() => checkMember(idx)}
+            />
           </th>
-          <th
-            className={`${textBaseNormal} flex felx-row text-left w-[120px] py-[13px] px-[8px] items-center`}
-          >
-            {member.name}
-          </th>
-          <th
-            className={`${textBaseNormal} flex felx-row text-left flex-1 py-[13px] px-[8px] items-center`}
-          >
-            {member.address}
-          </th>
-          <th
-            className={`${textBaseNormal} flex felx-row text-left flex-1 py-[13px] px-[8px] items-center`}
-          >
-            {member.memo}
-          </th>
-          <th
-            className={`${textBaseNormal} flex felx-row text-left w-[200px] py-[13px] px-[8px] items-center`}
-          >
-            {member.joinDate}
-          </th>
-          <th
-            className={`${textBaseNormal} flex felx-row text-left flex-1 py-[13px] px-[8px] items-center`}
-          >
-            {member.job}
-          </th>
-          <th className="flex felx-row text-left w-[150px] py-[13px] px-[8px] items-center">
-            <CheckBox />
-          </th>
-          <th className="flex felx-row text-left w-[48px] py-[13px] px-[8px]">
+          {DEFAULT_COLS.map((col, iidx) => {
+            return (
+              <th
+                key={`${col.name}-${col.filterKey}-${iidx}`}
+                className={`${textBaseNormal} ${tableWidth[iidx + 1]} py-[13px] px-[8px] items-center`}
+              >
+                {col.filterKey === 'emailAgreement' ? (
+                  <CheckBox
+                    checked={member.emailAgreement}
+                    onChange={() => checkEmailMember(idx)}
+                  />
+                ) : (
+                  member?.[col.filterKey]
+                )}
+              </th>
+            );
+          })}
+          <th className={`${tableWidth[7]} py-[13px] px-[8px]`}>
             <Button />
           </th>
         </tr>
