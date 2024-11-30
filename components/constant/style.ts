62ebdb77a941fe495611aa68eea763d25a39c5a6
HanKyeon <hgh21233@gmail.com>
2024-11-30 15:55:13 +0900

    feat: 공용 style 추가 및 수정

--- components/constant/style.ts
+++ components/constant/style.ts
@@ -6,4 +6,7 @@ export const textLgStrong =
   'font-semibold text-[18px] leading-[24px] text-right';
 export const textLgNormal = 'font-normal text-[18px] leading-[24px]';
 export const textBaseNormal = 'font-normal text-[14px] leading-[22px]';
-export const textBaseStrong = 'font-semibold text-sm leading-[22px]';
+export const textBaseStrong = 'font-semibold text-[16px] leading-[22px]';
+export const heading5 = 'font-semibold text-[16px] leading-[24px]';
+export const tableBorder =
+  'border-b-[1px] border-b-recatch-split bg-recatch-fill-alter';
