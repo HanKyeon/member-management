c9ba047672860b0c665c689cbf9f7b26f2baa0c2
HanKyeon <hgh21233@gmail.com>
2024-12-01 01:43:29 +0900

    feat: button type 추가 및 disabled 기능 추가

--- components/member/add-member-form/ModalFooter.tsx
+++ components/member/add-member-form/ModalFooter.tsx
@@ -1,14 +1,19 @@
 import CancelButton from '@/components/ui/CancelButton';
 import ConfirmButton from '@/components/ui/ConfirmButton';
+import { useFormContext } from 'react-hook-form';
 
 interface Props {
   cancelHandler: () => void;
 }
 
 const ModalFooter = function ({ cancelHandler }: Props) {
+  const {
+    formState: { isValid },
+  } = useFormContext();
   return (
     <footer className="w-full py-[12px] px-[16px] flex flex-row items-center justify-end gap-[10px] border-t-[1px] border-recatch-split bg-recatch-fill-alter">
       <CancelButton
+        type="button"
         className="px-[16px] py-[5px]"
         onClick={e => {
           e.preventDefault();
@@ -17,7 +22,13 @@ const ModalFooter = function ({ cancelHandler }: Props) {
       >
         취소
       </CancelButton>
-      <ConfirmButton className="px-[16px] py-[5px]">저장</ConfirmButton>
+      <ConfirmButton
+        type="submit"
+        className="px-[16px] py-[5px]"
+        disabled={!isValid}
+      >
+        저장
+      </ConfirmButton>
     </footer>
   );
 };
