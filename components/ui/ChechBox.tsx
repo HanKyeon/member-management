07198c4aa8293689a5841ca22d133c15abcfe3d4
HanKyeon <hgh21233@gmail.com>
2024-11-30 15:53:32 +0900

    feat: add props 'disabled' in checkbox
    - disabled 추가에 따른 class 변경사항 반영

--- components/ui/ChechBox.tsx
+++ components/ui/ChechBox.tsx
@@ -1,11 +1,18 @@
 import Check from '@/public/icons/Check.svg';
 
-const CheckBox = () => {
+interface Props {
+  className?: string;
+  disabled?: boolean;
+}
+
+const CheckBox = ({ className = '', disabled = false }: Props) => {
   return (
-    <div className="flex items-center justify-center w-[16px] h-[16px]">
+    <div
+      className={`flex items-center justify-center w-[16px] h-[16px] ${className}`}
+    >
       <label className="relative flex items-center justify-center w-full h-full cursor-pointer">
-        <input type="checkbox" className="sr-only peer" />
-        <div className="w-full h-full bg-recatch-text-light-solid border-[1px] border-gray-300 rounded-md peer-checked:bg-recatch-primary peer-checked:border-recatch-primary flex items-center justify-center transition-all duration-200">
+        <input type="checkbox" className="sr-only peer" disabled={disabled} />
+        <div className="w-full h-full bg-recatch-text-light-solid border-[1px] border-recatch-border rounded-[6px] peer-checked:bg-recatch-primary peer-checked:border-recatch-primary text-recatch-text-light-solid flex items-center justify-center transition-all duration-200 peer-disabled:bg-recatch-disabled peer-disabled:text-recatch-disabled peer-disabled:stroke-none">
           <Check />
         </div>
       </label>
