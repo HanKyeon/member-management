61e281b84942d609e48265b859f7e2f586305cf7
HanKyeon <hgh21233@gmail.com>
2024-11-30 13:23:39 +0900

    feat: add design system (confirm button)

---
+++ components/ui/ConfirmButton.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { ButtonHTMLAttributes, type PropsWithChildren } from 'react';
+import {
+  borderRadiusButton,
+  paddingSM,
+  textBaseNormal,
+} from '../constant/style';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+}
+
+const ConfirmButton = function ({
+  children,
+  disabled = false,
+  className,
+  ...rest
+}: Props) {
+  return (
+    <button
+      className={`${borderRadiusButton} ${paddingSM} ${textBaseNormal} text-recatch-text-light-solid bg-recatch-primary border-[1px] border-recatch-primary hover:bg-recatch-primary-hover hover:border-recatch-primary-hover active:bg-recatch-primary-active active:border-recatch-primary-active disabled:text-recatch-text-disabled disabled:bg-recatch-disabled disabled:border-recatch-border duration-200 ${className}`}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ConfirmButton;
