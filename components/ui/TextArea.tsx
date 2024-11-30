6283a019edc37697b3a8806073dc1903828daea6
HanKyeon <hgh21233@gmail.com>
2024-11-30 15:54:04 +0900

    feat: block text area resizing
    - textarea의 리사이징을 막는 class 추가

--- components/ui/TextArea.tsx
+++ components/ui/TextArea.tsx
@@ -15,7 +15,7 @@ const TextArea = function ({
 }: Props) {
   return (
     <textarea
-      className={`${borderRadiusLG} ${textBaseNormal} placeholder-recatch-text-placeholder border-[1px] border-recatch-border hover:border-recatch-primary focus:border-recatch-primary focus:shadow-input-blur text-recatch-text py-[5px] px-[12px] ${className}`}
+      className={`${borderRadiusLG} ${textBaseNormal} placeholder-recatch-text-placeholder border-[1px] border-recatch-border hover:border-recatch-primary focus:border-recatch-primary focus:shadow-input-blur text-recatch-text py-[5px] px-[12px] resize-none ${className}`}
       defaultValue={defaultValue}
       placeholder={placeholder}
       {...rest}
