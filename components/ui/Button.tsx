e3f53c9b94dcdecab9624a18d8c4aaddb92e1cde
HanKyeon <hgh21233@gmail.com>
2024-12-01 04:40:58 +0900

    feat: variant를 통한 추상화 및 재사용성 향상

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,24 +1,18 @@
 import MoreIcon from '@/public/icons/MoreOutlined.svg';
+import { ButtonProps } from '@/types/ui-types';
 
-import { borderRadiusButton, paddingSX } from '../constant/style';
-
-import type { ButtonHTMLAttributes } from 'react';
-
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  disabled?: boolean;
-  children?: React.ReactNode;
-  className?: string;
-}
+import { buttonClasses } from '@/utils/style-utils';
 
 const Button = function ({
   disabled = false,
   children = <MoreIcon />,
   className = '',
+  variant = 'more',
   ...rest
-}: Props) {
+}: ButtonProps) {
   return (
     <button
-      className={`${borderRadiusButton} ${paddingSX} text-recatch-text-hover hover:bg-recatch-text-hover active:bg-recatch-text-hover disabled:bg-recatch-text-hover duration-200 ${className}`}
+      className={buttonClasses[variant](className)}
       disabled={disabled}
       {...rest}
     >
