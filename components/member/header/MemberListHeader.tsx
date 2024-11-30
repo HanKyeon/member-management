5b00fd64dfb26c3ace32d9b81a6cfdb7243fc13e
HanKyeon <hgh21233@gmail.com>
2024-12-01 01:43:11 +0900

    chore: 불필요한 Fragment 제거

--- components/member/header/MemberListHeader.tsx
+++ components/member/header/MemberListHeader.tsx
@@ -10,18 +10,16 @@ interface Props {
 
 const MemberListHeader = function ({ buttonClick, buttonText, title }: Props) {
   return (
-    <>
-      <header className="w-full py-[12px] px-[14px] flex flex-row items-center flex-shrink-0">
-        <h1 className={`${heading5} block flex-1`}>{title}</h1>
-        <ConfirmButton
-          className="flex-shrink-0 flex flex-row items-center gap-[10px]"
-          onClick={buttonClick}
-        >
-          <Plus />
-          {buttonText}
-        </ConfirmButton>
-      </header>
-    </>
+    <header className="w-full py-[12px] px-[14px] flex flex-row items-center flex-shrink-0">
+      <h1 className={`${heading5} block flex-1`}>{title}</h1>
+      <ConfirmButton
+        className="flex-shrink-0 flex flex-row items-center gap-[10px]"
+        onClick={buttonClick}
+      >
+        <Plus />
+        {buttonText}
+      </ConfirmButton>
+    </header>
   );
 };
 
