6b28a285bcd2df9e27b7ddcd5f33e5ce4434bdac
HanKyeon <hgh21233@gmail.com>
2024-11-30 18:18:50 +0900

    feat: add useFilterMap
    - 필터를 Set과 Map 2차원으로 저장하여 관리하도록 추가

---
+++ hooks/useFilterMap.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+export type Filters = {
+  name: string;
+  address: string;
+  memo: string;
+  joinDate: string;
+  job: '개발자' | 'PO' | '디자이너';
+  emailAgreement: boolean;
+};
+
+export type FilterKey = keyof Filters;
+export type FilterValue = Filters[FilterKey];
+
+export const useFilterMap = function (
+  initialFilter: Partial<Record<FilterKey, FilterValue[]>> = {
+    name: [],
+    address: [],
+    memo: [],
+    joinDate: [],
+    job: [],
+    emailAgreement: [],
+  },
+) {
+  const [filterMap, setFilterMap] = useState<Map<FilterKey, Set<FilterValue>>>(
+    new Map<FilterKey, Set<FilterValue>>(
+      Object.entries(initialFilter).map(([key, value]) => [
+        key as FilterKey,
+        new Set(value),
+      ]),
+    ),
+  );
+
+  const addFilter = function (key: FilterKey, value: FilterValue) {
+    setFilterMap(prev => {
+      const updatedMap = new Map(prev);
+      const existingSet = updatedMap.get(key) || new Set();
+      updatedMap.set(key, new Set([...existingSet, value]));
+      return updatedMap;
+    });
+  };
+
+  const removeFilter = function (key: FilterKey, value: FilterValue) {
+    setFilterMap(prev => {
+      const updatedMap = new Map(prev);
+      const existingSet = updatedMap.get(key);
+      if (existingSet) {
+        existingSet.delete(value);
+      }
+      updatedMap.set(key, new Set(existingSet));
+      return updatedMap;
+    });
+  };
+
+  const toggleFilter = function (key: FilterKey, value: FilterValue) {
+    setFilterMap(prev => {
+      const updatedMap = new Map(prev);
+      const existingSet = updatedMap.get(key) || new Set();
+      if (existingSet.has(value)) {
+        existingSet.delete(value);
+        console.log('제거');
+      } else {
+        existingSet.add(value);
+        console.log('추가');
+      }
+      updatedMap.set(key, new Set([...existingSet]));
+      return updatedMap;
+    });
+  };
+
+  return {
+    filterMap,
+    addFilter,
+    removeFilter,
+    toggleFilter,
+  };
+};
