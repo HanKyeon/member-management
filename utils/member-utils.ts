aad74fcb5195ca07ae86d531fbae3622c2a72ca2
HanKyeon <hgh21233@gmail.com>
2024-11-30 21:49:04 +0900

    chore: add JSDoc in utils

--- utils/member-utils.ts
+++ utils/member-utils.ts
@@ -1,5 +1,11 @@
 import { MemberRecord } from '@/types/type';
 
+/**
+ * 두 멤버 비교 후 같은지 반환
+ * @param {MemberRecord} member1
+ * @param {MemberRecord} member2
+ * @returns {boolean}
+ */
 export const isEqual = function (member1: MemberRecord, member2: MemberRecord) {
   const keys1 = Object.keys(member1);
   const keys2 = Object.keys(member2);
--- utils/storage-utils.ts
+++ utils/storage-utils.ts
@@ -2,6 +2,10 @@ import { MemberRecord } from '@/types/type';
 
 const isPersist = process.env.NEXT_PUBLIC_STORAGE === 'local-storage';
 
+/**
+ * 데이터를 로컬 스토리지에 저장
+ * @param {MemberRecord[]} data - 로컬 스토리지에 저장할 MemberRecord[]
+ */
 export const setRecordsInLocalStorage = function (data: MemberRecord[]) {
   if (isPersist) localStorage.setItem('records', JSON.stringify(data));
 };
