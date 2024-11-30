658c9a6710e2ca5002eeb1aa3a0cb68c06e7b335
HanKyeon <hgh21233@gmail.com>
2024-11-30 18:39:20 +0900

    feat: input 의 다른 attr 추가
    - checkbox input과 peer하게 ui를 구성하였기에 input의 props를 받도록 했습니다.

--- components/ui/ChechBox.tsx
+++ components/ui/ChechBox.tsx
@@ -1,17 +1,24 @@
+import { InputHTMLAttributes } from 'react';
+
 import Check from '@/public/icons/Check.svg';
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   disabled?: boolean;
 }
 
-const CheckBox = ({ className = '', disabled = false }: Props) => {
+const CheckBox = ({ className = '', disabled = false, ...rest }: Props) => {
   return (
     <div
       className={`flex items-center justify-center w-[16px] h-[16px] ${className}`}
     >
       <label className="relative flex items-center justify-center w-full h-full cursor-pointer">
-        <input type="checkbox" className="sr-only peer" disabled={disabled} />
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          disabled={disabled}
+          {...rest}
+        />
         <div className="w-full h-full bg-recatch-text-light-solid border-[1px] border-recatch-border rounded-[6px] peer-checked:bg-recatch-primary peer-checked:border-recatch-primary text-recatch-text-light-solid flex items-center justify-center transition-all duration-200 peer-disabled:bg-recatch-disabled peer-disabled:text-recatch-disabled peer-disabled:stroke-none">
           <Check />
         </div>
