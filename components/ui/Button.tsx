1364e3d28d9df25d1534bf54ab1b5c985d99cef6
HanKyeon <hgh21233@gmail.com>
2024-11-30 13:23:20 +0900

    feat: add design system (button)

---
+++ components/ui/Button.tsx
@@ -0,0 +1,28 @@
+import MoreIcon from '@/public/icons/MoreOutlined.svg';
+import { borderRadiusButton, paddingSX } from '../constant/style';
+import { ButtonHTMLAttributes } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const Button = function ({
+  disabled = false,
+  children = <MoreIcon />,
+  className = '',
+  ...rest
+}: Props) {
+  return (
+    <button
+      className={`${borderRadiusButton} ${paddingSX} text-recatch-text-hover hover:bg-recatch-text-hover active:bg-recatch-text-hover disabled:bg-recatch-text-hover duration-200 ${className}`}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
