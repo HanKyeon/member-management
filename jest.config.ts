78712bc566c425ef668790d57ac8dde26a41a33a
HanKyeon <hgh21233@gmail.com>
2024-12-01 08:32:55 +0900

    feat: jest setting 추가

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
+
+module.exports = createJestConfig(customJestConfig);
