35442ea7d0c66754786203d6ba0d21a05dd5193c
HanKyeon <hgh21233@gmail.com>
2024-11-30 23:52:39 +0900

    feat: add form resolver

---
+++ utils/form-utils.ts
@@ -0,0 +1,19 @@
+import { MemberRecord } from '@/types/type';
+
+export const MemberFormResolver = (data: MemberRecord) => {
+  const errors: Record<string, { type?: string; message: string }> = {};
+  if (!data.name) {
+    errors.name = {
+      type: 'required',
+      message: '이름은 필수값입니다.',
+    };
+  }
+  if (!data.joinDate) {
+    errors.joinDate = {
+      type: 'required',
+      message: '가입일은 필수값입니다.',
+    };
+  }
+  const values = Object.keys(errors).length === 0 ? data : {};
+  return { errors, values };
+};
