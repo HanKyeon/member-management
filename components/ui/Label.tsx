726ffa8768cb1cbeedff507fac418c039e39b37b
HanKyeon <hgh21233@gmail.com>
2024-11-30 13:24:54 +0900

    feat: add design system (label)

---
+++ components/ui/Label.tsx
@@ -0,0 +1,24 @@
+import { LabelHTMLAttributes } from 'react';
+import { textLgNormal, textLgStrong } from '../constant/style';
+
+interface Props extends LabelHTMLAttributes<HTMLLabelElement> {
+  text?: string;
+  required?: boolean;
+  className?: string;
+}
+
+const Label = function ({ required, text, className = '', ...rest }: Props) {
+  return (
+    <label
+      className={`${textLgStrong} text-recatch-text-tertiary ${className}`}
+      {...rest}
+    >
+      {text}{' '}
+      {required && (
+        <span className={`${textLgNormal} text-recatch-error`}>*</span>
+      )}
+    </label>
+  );
+};
+
+export default Label;
