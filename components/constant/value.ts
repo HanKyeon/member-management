ee780a690defcca1bc7fa9cc0c83e8805982a1bb
HanKyeon <hgh21233@gmail.com>
2024-11-30 23:17:24 +0900

    feat: form element map을 위한 값 추가

--- components/constant/value.ts
+++ components/constant/value.ts
@@ -4,13 +4,20 @@ import { Job, MemberRecord } from '@/types/type';
 export const DEFAULT_COLS: {
   name: string;
   filterKey: FilterKey;
+  required?: boolean;
+  className?: string;
 }[] = [
-  { filterKey: 'name', name: '이름' },
-  { filterKey: 'address', name: '주소' },
-  { filterKey: 'memo', name: '메모' },
-  { filterKey: 'joinDate', name: '가입일' },
-  { filterKey: 'job', name: '직업' },
-  { filterKey: 'emailAgreement', name: '이메일 수신 동의' },
+  { filterKey: 'name', name: '이름', required: true, className: '' },
+  { filterKey: 'address', name: '주소', required: false, className: '' },
+  { filterKey: 'memo', name: '메모', required: false, className: '' },
+  { filterKey: 'joinDate', name: '가입일', required: true, className: 'w-1/3' },
+  { filterKey: 'job', name: '직업', required: false, className: 'w-1/3' },
+  {
+    filterKey: 'emailAgreement',
+    name: '이메일 수신 동의',
+    required: false,
+    className: '',
+  },
 ];
 
 export const DEFAULT_DATA: MemberRecord[] = [
