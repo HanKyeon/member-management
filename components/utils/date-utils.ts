f87d3714d3e68dbc898273c1b4e60ae45170edce
HanKyeon <hgh21233@gmail.com>
2024-11-30 13:25:22 +0900

    feat: add date formatter
    - yyyy-MM-dd로 포매팅하는 유틸 함수 추가

--- components/utils/date-utils.ts
+++ components/utils/date-utils.ts
@@ -45,3 +45,16 @@ export const getStartDayIndex = (
   const weekdays = getWeekdayNames(locale);
   return weekdays.indexOf(weekday);
 };
+
+/**
+ * yyyy-MM-dd 포매터
+ * @param {Date} date - Date
+ * @returns {string} - yyyy-MM-dd
+ */
+export const formatDate = (date: Date): string => {
+  const year = date.getFullYear(); // 연도
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 월 (0부터 시작하므로 +1)
+  const day = String(date.getDate()).padStart(2, '0'); // 일
+
+  return `${year}-${month}-${day}`;
+};
