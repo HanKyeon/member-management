603718aec2bb7be7a3fabcc2b8a3c2ba69b808a1
HanKyeon <hgh21233@gmail.com>
2024-11-30 13:24:46 +0900

    feat: add design system (input)

---
+++ components/ui/Input.tsx
@@ -0,0 +1,26 @@
+import { type InputHTMLAttributes } from 'react';
+import { borderRadiusButton, textBaseNormal } from '../constant/style';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  placeholder?: string;
+  defaultValue?: string;
+}
+
+const Input = function ({
+  className = '',
+  placeholder,
+  defaultValue,
+  ...rest
+}: Props) {
+  return (
+    <input
+      className={`${borderRadiusButton} ${textBaseNormal} placeholder-recatch-text-placeholder border-[1px] border-recatch-border hover:border-recatch-primary focus:border-recatch-primary focus:shadow-input-blur text-recatch-text py-[5px] px-[12px] ${className}`}
+      defaultValue={defaultValue}
+      placeholder={placeholder}
+      {...rest}
+    />
+  );
+};
+
+export default Input;
