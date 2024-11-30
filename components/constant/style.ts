e107dc6369d1e922cc04515a267b5776a6e0f2fb
HanKyeon <hgh21233@gmail.com>
2024-11-30 10:35:21 +0900

    feat: add styles
    - 공통으로 사용되는 style 추가

--- components/constant/style.ts
+++ components/constant/style.ts
@@ -1,3 +1,8 @@
 export const paddingSM = 'py-[5px] px-[12px]';
 export const paddingSX = 'p-[8px]';
 export const borderRadiusLG = 'rounded-[10px]';
+export const borderRadiusButton = 'rounded-[8px]';
+export const textLgStrong =
+  'font-semibold text-[18px] leading-[24px] text-right';
+export const textLgNormal = 'font-normal text-[18px] leading-[24px]';
+export const textBaseNormal = 'font-normal text-[14px] leading-[22px]';
