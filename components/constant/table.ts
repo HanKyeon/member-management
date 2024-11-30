b6e2f97696fde3e707ded3d5f02665173e0801fd
HanKyeon <hgh21233@gmail.com>
2024-11-30 16:41:48 +0900

    feat: add table constant
    - 테이블 너비 스타일 constant 추가

---
+++ components/constant/table.ts
@@ -0,0 +1,10 @@
+export const tableWidth = [
+  'text-left flex-shrink-0 flex items-center justify-center',
+  'flex felx-row text-left w-[120px]',
+  'flex felx-row text-left flex-1',
+  'flex felx-row text-left flex-1',
+  'flex felx-row text-left w-[200px]',
+  'flex felx-row text-left flex-1',
+  'flex felx-row text-left w-[150px]',
+  'flex felx-row text-left w-[48px]',
+];
